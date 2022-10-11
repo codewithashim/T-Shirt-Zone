1a408157aa2e6cef19b7113914c0787679889943
codewithashim <codewithashim@gmail.com>
2022-10-11 12:46:45 +0600

    adde cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, hendelRemoveFromCart }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <h3>Cart is Empty</h3>;
+  } else if (cart.length === 1) {
+    message = <h3>Just Adde One Product</h3>;
+  } else {
+    message = <h3>Thenks For Buying</h3>;
+  }
+
   return (
     <div>
-      <h2>Order Summary</h2>
-      <h4>Items Ordered: {cart.length}</h4>
+      <h2 className={cart.length === 0 ? `red`:`oreng`}>Order Summary</h2>
+      <h4 className={`bold ${ cart.length===7 ? `green`:`gry`}`}>Items Ordered: {cart.length}</h4>
+      {cart.map((item) => {
+        return (
+          <div className="cartItem">
+            <h3>{item.name}</h3>
+            <h4>Price: ${item.price}</h4>
+            <button
+              className="removeBtn"
+              onClick={() => hendelRemoveFromCart(item)}
+            >
+              X
+            </button>
+          </div>
+        );
+      })}
+      {message}
+      {cart.length === 3 ? (
+        <h3>Thanks For Buying 3 T-shirt</h3>
+      ) : (
+        <h3>Buye More Get More Offer</h3>
+      )}
+      {cart.length === 2 && <h3>Thanks For Buying 2 T-shirt</h3>}
+      {cart.length === 5 || <h3>Thanks For Buying 5 T-shirt</h3>}
     </div>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,20 +9,41 @@ const Home = () => {
   const product = useLoaderData();
   const [cart, setCart] = useState([]);
 
-  const hendelAddToCart =(tshirt)=>{
+  const hendelAddToCart = (tshirt) => {
+    const exist = cart.find((item) => item._id === tshirt._id);
+    if (exist) {
+      alert("This item is already added to cart");
+    } else {
       const newCart = [...cart, tshirt];
       setCart(newCart);
-  }
+    }
+  };
+
+  const hendelRemoveFromCart = (tshirt) => {
+    const reminingCart = cart.filter((ts) => ts._id !== tshirt._id);
+    console.log(reminingCart);
+    setCart(reminingCart);
+  };
 
   return (
     <section className="mainHomeSection">
       <div className="t-ShirtContainer">
         {product.map((tshirt) => {
-          return <Products key={tshirt._id} tshirt={tshirt} hendelAddToCart={hendelAddToCart}></Products>;
+          return (
+            <Products
+              key={tshirt._id}
+              tshirt={tshirt}
+              hendelAddToCart={hendelAddToCart}
+            ></Products>
+          );
         })}
       </div>
       <div className="orderSummary">
-        <Cart cart={cart}></Cart>
+        <Cart
+          key={cart._id}
+          cart={cart}
+          hendelRemoveFromCart={hendelRemoveFromCart}
+        ></Cart>
       </div>
     </section>
   );
